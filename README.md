a5ee3a0450867c1eedc1715120bd3aa8cdd3373f
mawathilde <dev@mawathilde.fr>
2024-03-27 10:25:40 +0000

    feat: Init auth api

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Madeline's Journey
 
 ![madeline](https://i.pinimg.com/474x/f1/ee/e4/f1eee47021845369982abf61c629c7d6.jpg)
+
+WIP: Setup api:
+- Auth: https://medium.com/readytowork-org/secure-your-go-web-application-jwt-authentication-e65a5af7c049
---
+++ api/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"madeline-journey/api/controllers"
+	"madeline-journey/api/db"
+
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	db.LoadEnvVariables()
+	db.ConnectToDb()
+	db.SyncDatabase()
+}
+
+func main() {
+	r := gin.Default()
+
+	r.POST("/signup", controllers.Signup)
+	r.POST("/login", controllers.Login)
+	//r.GET("/validate", middleware.RequireAuth ,controllers.Validate).  // here RequireAuth is a middleware that we will be creating below. It protects the route
+
+	r.Run()
+}
