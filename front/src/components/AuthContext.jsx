c69e6a36011beea1003e97700c2b2c8caf8cdbb4
mawathilde <dev@mawathilde.fr>
2024-04-10 13:18:48 +0000

    fix: change setToken in AuthProvider

--- front/src/components/AuthContext.jsx
+++ front/src/components/AuthContext.jsx
@@ -3,17 +3,17 @@ import React, { createContext, useState, useEffect } from 'react';
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-	const [token, setToken] = useState(null);
+	const [token, setContextToken] = useState(null);
 	const [loading, setLoading] = useState(true);
 
 	useEffect(() => {
 		const storedToken = localStorage.getItem('token');
-		setToken(storedToken);
+		setContextToken(storedToken);
 		setLoading(false);
 	}, []);
 
 	return (
-		<AuthContext.Provider value={{ token, setToken, loading }}>
+		<AuthContext.Provider value={{ token, setContextToken, loading }}>
 			{children}
 		</AuthContext.Provider>
 	);
